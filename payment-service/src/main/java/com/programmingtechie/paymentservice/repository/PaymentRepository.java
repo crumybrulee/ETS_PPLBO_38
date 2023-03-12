e72839838b637704a79e4083f3a72f829ca23c8d
Caturbintari <caturianibintari36@gmail.com>
2023-03-12 10:41:00 +0700

    hapus

--- payment-service/src/main/java/com/programmingtechie/paymentservice/repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.programmingtechie.paymentservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.programmingtechie.paymentservice.model.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
-}
